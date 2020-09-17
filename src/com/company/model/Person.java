3a776eb0d9bbb0f5225077f664f9792a39c3fb6a
Robin <robin.aahlin@gmail.com>
2020-09-17 13:55:28 +0100

    Person model added.

---
+++ src/com/company/model/Person.java
@@ -0,0 +1,38 @@
+package com.company.model;
+
+public class Person {
+
+    private String name;
+    private int points;
+    private int matches;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getMatches() {
+        return matches;
+    }
+
+    public void setMatches(int matches) {
+        this.matches = matches;
+    }
+
+    public Person(String name){
+        this.name = name;
+        this.matches = 0;
+        this.points = 0;
+    }
+}
